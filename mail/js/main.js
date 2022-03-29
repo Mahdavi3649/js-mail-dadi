4cc4bdda7527854c9409accd27ae3ab22e6cf872
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-29 17:48:15 +0200

    Bonus fatto! aggiustato e completato

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,28 +1,34 @@
 /* Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. */
 
 
-const listEmail = ["ivadham.smay3649@gmail.com", "mymail@gmail.com","john-s@yahoo.com"];
 //const mailUtente = prompt("Insert your mail Address?!");
 
 
-let answer = document.getElementById("mail").value;
+let answer = document.getElementById("answer");
+const send = document.getElementById("send");
 
-const send = document.getElementById("send")
 send.addEventListener("click", function() {
     
+    let success = false;
+    const listEmail = ["ivadham.smay3649@gmail.com", "mymail@gmail.com","john-s@yahoo.com"];
     const mailUtente = document.getElementById("mail").value;
 
     for(let i = 0; i < listEmail.length; i++) {
 
         if(mailUtente === listEmail[i]){
-        //Se l'utente è nella mia lista stampa "L'indirizzo email è corretto."
-           let correct = "Your mail address is correct";
-           answer.innerHTML = correct;
-        } else{
-            let uncorrect = "Sorry, your mail address does not exist"
-            answer.innerHTML = uncorrect;
+           success = true;
+        } 
+    }
 
-        }
+    if(success) {
+        // Se l'utente è nella mia lista stampa "L'indirizzo email è corretto."
+        let correct = "Your mail address is correct";
+        answer.innerHTML = correct;
+
+    } else {
+         let uncorrect = "Sorry, your mail address does not exist"
+          answer.innerHTML = uncorrect;
+
     }
 });
 
