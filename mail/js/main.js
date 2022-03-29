34b8ba9c14764b4d62ea243d24a38b97563f7a04
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-29 16:19:48 +0200

    facendo bonus

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,20 +2,30 @@
 
 
 const listEmail = ["ivadham.smay3649@gmail.com", "mymail@gmail.com","john-s@yahoo.com"];
-const mailUtente = prompt("Insert your mail Address?!");
+//const mailUtente = prompt("Insert your mail Address?!");
+const mailUtente = document.getElementById("mail");
 
-let answer = document.getElementById("answer");
 
-// Stampa un messaggio appropriato sull’esito del controllo.
-answer.innerHTML = "Sorry, your mail address does not exist"
+let answer = document.getElementById("mail");
 
-for(let i = 0; i < listEmail.length; i++) {
 
-    if(mailUtente === listEmail[i]){
-    //Se l'utente è nella mia lista stampa "L'indirizzo email è corretto."
-       let correct = "Your mail address is correct";
-       answer.innerHTML = correct;
+answer.addEventListener("click", function() {
+    for(let i = 0; i < listEmail.length; i++) {
+
+        if(mailUtente === listEmail[i]){
+        //Se l'utente è nella mia lista stampa "L'indirizzo email è corretto."
+           let correct = "Your mail address is correct";
+           answer.innerHTML = correct;
+        } else{
+            let uncorrect = "Sorry, your mail address does not exist"
+            answer.innerHTML = uncorrect;
+
+        }
     }
-}
+});
+
+
+
+
 
 
